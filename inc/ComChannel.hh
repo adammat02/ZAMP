9a7fca55dd567a3caca3feae4fb0e56a9ca7122f
Adam Matusiak <275460@student.pwr.edu.pl>
2025-11-11 18:57:40 +0100

    dodanie parsera xml

---
+++ inc/ComChannel.hh
@@ -0,0 +1,8 @@
+#ifndef COMCHANNEL_HH
+#define COMCHANNEL_HH
+
+#include "AbstractComChannel.hh"
+
+class ComChannel: public AbstractComChannel {};
+
+#endif
---
+++ inc/Scene.hh
@@ -0,0 +1,8 @@
+#ifndef SCENE_HH
+#define SCENE_HH
+
+#include "AbstractScene.hh"
+
+class Scene: public AbstractScene {};
+
+#endif
