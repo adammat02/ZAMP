3bfdc724d768bd49a932211fa5e18f330ca56234
Adam Matusiak <275460@student.pwr.edu.pl>
2025-11-18 14:26:30 +0100

    Klasa Scene

--- inc/Scene.hh
+++ inc/Scene.hh
@@ -2,7 +2,36 @@
 #define SCENE_HH
 
 #include "AbstractScene.hh"
+#include "AbstractMobileObj.hh"
+#include <string>
+#include <map>
+#include <memory>
 
-class Scene: public AbstractScene {};
+class Scene : public AbstractScene
+{
+  typedef std::map<std::string, std::shared_ptr<AbstractMobileObj>> ObjMap;
+
+  ObjMap _Set_MobileObjs;
+
+public:
+  ~Scene();
+  /*!
+   * \brief Wyszukuje obiekt o podanej pełnej kwalifikowanej nazwie.
+   *
+   *  Wyszukuje obiekt o podanej pełnej kwalifikowanej nazwie.
+   *  \param[in] sName - pełna kwalifikowana nazwa obiektu.
+   *  \return Jeśli obiekt zostanie znaleziony, to zwracany jest wskaźnik
+   *          na niego. W przypadku przeciwnym zwracany jest wskaźnik \p nullptr.
+   */
+  AbstractMobileObj *FindMobileObj(const char *sName) const;
+  /*!
+   * \brief Dodaje do sceny nowy obiekt.
+   *
+   *  Dodaje do sceny nowy obiekt. Obiekt ten musi mieć poprawnie
+   *  skonstruowaną pełną kwalifikowaną nazwę.
+   *  \param[in] pMobObj - wskaźnik na istniejący obiekt.
+   */
+  void AddMobileObj(AbstractMobileObj *pMobObj);
+};
 
 #endif
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,1 +1,31 @@
 #include "Scene.hh"
+
+AbstractMobileObj *Scene::FindMobileObj(const char *sName) const
+{
+  ObjMap::const_iterator it = _Set_MobileObjs.find(sName);
+  if (it != _Set_MobileObjs.end())
+  {
+    return it->second.get();
+  }
+  return nullptr;
+}
+
+void Scene::AddMobileObj(AbstractMobileObj *pMobObj)
+{
+  if (!pMobObj)
+  {
+    std::cerr << "!!! Wskaźnik do obiektu mobilnego jest nullptr." << std::endl;
+    return;
+  }
+
+  std::string name = pMobObj->GetName();
+  if (name.empty())
+  {
+    std::cerr << "!!! Niepoprawna nazwa obiektu mobilnego." << std::endl;
+    return;
+  }
+
+  std::shared_ptr<AbstractMobileObj> pObj = std::shared_ptr<AbstractMobileObj>(pMobObj);
+
+  _Set_MobileObjs[name] = pObj;
+}
